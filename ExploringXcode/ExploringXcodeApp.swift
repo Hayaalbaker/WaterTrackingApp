b620e196b8f2140e122f7f8aead2876c2016a7e9
Hayaalbaker <144670605+Hayaalbaker@users.noreply.github.com>
2024-09-25 16:21:43 +0300

    Initial Commit

---
+++ ExploringXcode/ExploringXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploringXcodeApp.swift
+//  ExploringXcode
+//
+//  Created by Haya Albaker on 25/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExploringXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
