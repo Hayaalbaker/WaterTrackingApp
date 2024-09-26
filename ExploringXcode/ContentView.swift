c2f8cf7f02feab7bd46614a52e6ea34e20b1eaa2
Hayaalbaker <144670605+Hayaalbaker@users.noreply.github.com>
2024-09-26 17:39:31 +0300

    stepper fixed

--- ExploringXcode/ContentView.swift
+++ ExploringXcode/ContentView.swift
@@ -7,21 +7,20 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View { @State private var stepvalue: Int = 0
     var body: some View {
         VStack {
                 Text("Water tracker 💦")
             Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
                 /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Apple Health")/*@END_MENU_TOKEN@*/ }
-                Stepper(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(4)/*@END_MENU_TOKEN@*/, in: 0...100) {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Cups to drink per day")/*@END_MENU_TOKEN@*/
+            Stepper("Cups to drink per day: \(stepvalue)", value: $stepvalue, in: 0...100) {_ in
+                    
                     }
 
             Button(/*@START_MENU_TOKEN@*/"Continue"/*@END_MENU_TOKEN@*/) {
                 /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
             }
-            Text("Water tracker 💦")
-            
+                
         }
         .padding()
     }
