3216819e35ad222f6f25453e32163badaaf416dc
Hayaalbaker <144670605+Hayaalbaker@users.noreply.github.com>
2024-09-26 17:29:25 +0300

    testing

--- ExploringXcode/ContentView.swift
+++ ExploringXcode/ContentView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                Text("Water tracker 💦")
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Apple Health")/*@END_MENU_TOKEN@*/ }
+                Stepper(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(4)/*@END_MENU_TOKEN@*/, in: 0...100) {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Cups to drink per day")/*@END_MENU_TOKEN@*/
+                    }
+
+            Button(/*@START_MENU_TOKEN@*/"Continue"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            Text("Water tracker 💦")
+            
         }
         .padding()
     }
