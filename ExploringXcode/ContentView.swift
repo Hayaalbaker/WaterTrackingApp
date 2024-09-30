a65ed86cca82423752bd374e2366a8d46675ca8c
Hayaalbaker <144670605+Hayaalbaker@users.noreply.github.com>
2024-09-30 14:33:09 +0300

    state/binding workshop

--- ExploringXcode/ContentView.swift
+++ ExploringXcode/ContentView.swift
@@ -7,15 +7,17 @@
 
 import SwiftUI
 
-struct ContentView: View { @State private var stepvalue: Int = 0
+struct ContentView: View {
+    @State private var stepvalue: Int = 0
+    @State var AppleHealth: Bool = true
+   var noOfCups: Int = 8
     var body: some View {
         VStack {
-                Text("Water tracker 💦")
-            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+            Text("Water tracker 💦")
+            Toggle(isOn: $AppleHealth) {
                 /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Apple Health")/*@END_MENU_TOKEN@*/ }
-            Stepper("Cups to drink per day: \(stepvalue)", value: $stepvalue, in: 0...100) {_ in
-                    
-                    }
+            
+            Stepper("Cups to drink per day: \(stepvalue)", value: $stepvalue, in: 0...100) {_ in}
 
             Button(/*@START_MENU_TOKEN@*/"Continue"/*@END_MENU_TOKEN@*/) {
                 /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
